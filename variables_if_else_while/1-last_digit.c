4a9b21919f147248327b73ade4e0116d1586e34f
hewsontrinh526 <6734@holbertonstudents.com>
2023-06-19 03:08:54 -0700

    Creating a program that will assign a random number to a variable and then print out whether the last digit of that variable is greater than 5, is 0 or is less than 6 and not 0

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ int main(void)
 	}
 	else if (digit == 0)
 	{
-		printf("Last digiti of %d is %d and is 0\n", n, digit);
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 	}
 	return (0);
 }
