888d8a126a74ed541cd4e3ace33d574b9252ba01
hewsontrinh526 <6734@holbertonstudents.com>
2023-06-20 00:12:18 -0700

    Creating a function that computes the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Checks for lowercase characters.
+ *
+ * @n: char input from user.
+ *
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
