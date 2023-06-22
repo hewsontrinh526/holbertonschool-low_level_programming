050fbba0c3254267257e1d079981fa7bfa3d73f2
hewsontrinh526 <6734@holbertonstudents.com>
2023-06-22 01:20:02 -0700

    WRiting a function tha tprints the numbers from 0 to 9, followed by a new line

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Multiplies two digits together.
+ *
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		_putchar(n + '0');
+		n = n + 1;
+	}
+	_putchar('\n');
+}
