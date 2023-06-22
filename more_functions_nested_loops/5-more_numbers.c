500e8fb8cee096e403433caa37bbdb402802728a
hewsontrinh526 <6734@holbertonstudents.com>
2023-06-22 02:22:58 -0700

    Writing a function that prints 10 times the numbers from 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints the numbers from 0 to 9 except for 2 and 4..
+ * more_numbers - Prints the numbers from 0 to 14, 10 times.
  *
  *
  * Return: Always 0.
