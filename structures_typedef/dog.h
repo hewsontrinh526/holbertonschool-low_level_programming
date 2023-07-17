a62179d0849b7510dc543c681659c7cc1a3804b9
hewsontrinh526 <6734@holbertonstudents.com>
2023-07-16 17:57:17 -0700

    Defining a new structure named dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - The description of the dog
+ *
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
