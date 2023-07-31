73c271143bf5fc9d246724519fb232f4ac400814
hewsontrinh526 <6734@holbertonstudents.com>
2023-07-31 02:24:04 -0700

    A function that is executed before the main function

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-void before_main() __attribute__((constructor));
+void before_main(void) __attribute__((constructor));
 
-void before_main()
+/**
+ * before_main - A function that executes before the main function
+ *
+ * Return: void
+ */
+
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
