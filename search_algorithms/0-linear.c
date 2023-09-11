1f2b2608be87ff0cb8a9be1c350175cf88836a47
hewsontrinh526 <6734@holbertonstudents.com>
2023-09-11 16:21:04 -0700

    Feat: Adding linear search function

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Search for a value in an array using
+ * using the linear search method
+ *
+ * @array: Array to be searched
+ * @size: Size of array
+ * @value: Value to be found
+ *
+ * Return: Index of value if found, else -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	i = 0;
+
+	while (i < size - 1)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		printf("Value checked array[%li] = [%d]\n", i, array[i]);
+		i = i + 1;
+	}
+	return (-1);
+}
